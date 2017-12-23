20f9699cc7f77368846616f5bd8dc22fc49e692d
Şevket Kaplan <sevketkaplan.20@gmail.com>
2017-12-23 22:33:03 +0000

    Rails Blog Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:image])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password, :image])
   end
 
 end
